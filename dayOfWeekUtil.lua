6d485cc7fc7eb7d310a4e39a9a00a6c45484acf5
Jos Kuijpers <jos@kuijpersvof.nl>
2016-11-08 15:52:25 +0100

    Clean up file, improve code of dayOfWeekUtil
    - Cleaner consistent code style
    - Removed spaces at end of lines
    - Using modulo is faster, branching is slower, also it is (in my opinion) more readable.

--- dayOfWeekUtil.lua
+++ dayOfWeekUtil.lua
@@ -2,7 +2,7 @@
 ---------------------------------------------------------------------------------------------------------
 -- DAYOFWEEKUTIL SCRIPT
 ---------------------------------------------------------------------------------------------------------
--- Purpose:   
+-- Purpose:  Calculate current day of the week using gametime (Mon-Sun)
 -- Authors:  theSeb, Akuenzi
 --
 
@@ -14,11 +14,10 @@ DayOfWeekUtil.daysInWeek = 7;
 
 function DayOfWeekUtil:loadMap(name)
      print("Loading DayOfWeekUtil");
-     g_currentMission.DayOfWeekUtil = self;  
-       
+     g_currentMission.DayOfWeekUtil = self;
 end;
 
-function DayOfWeekUtil:deleteMap() 
+function DayOfWeekUtil:deleteMap()
 end;
 
 function DayOfWeekUtil:mouseEvent(posX, posY, isDown, isUp, button)
@@ -30,33 +29,26 @@ end;
 function DayOfWeekUtil:update(dt)
 end;
 
-function DayOfWeekUtil:draw() 
+function DayOfWeekUtil:draw()
 end;
 
 --assumes that day 1 = monday
 function DayOfWeekUtil:CalculateDayofWeekBasedOnDayNumber(dayNumber)
-    
     local dayOfWeek = dayNumber; -- this will work for days 1 to 6
 
     if (dayNumber % self.daysInWeek == 0) then -- if it's a perfect multiple of 7'
-        dayOfWeek = 7; -- will always be sunday 
+        dayOfWeek = 7; -- will always be sunday
     elseif (dayNumber > self.daysInWeek) then
-        local weekNumber = math.floor(dayNumber/self.daysInWeek); 
+        local weekNumber = math.floor(dayNumber/self.daysInWeek);
         dayOfWeek = dayNumber - (weekNumber * self.daysInWeek);
     end;
 
     return dayOfWeek;
-
 end;
 
 --might end up not using this function
 function DayOfWeekUtil:ReturnNextDayNumber(currentDay)
-    if currentDay == 7 then
-        return 1;
-    else
-        return currentDay + 1;
-    end;
+    return (currentDay + 1) % self.daysInWeek;
 end;
-
 
 addModEventListener(DayOfWeekUtil);
