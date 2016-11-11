824bb1c6d3360667738e46c56d8dc97cec32436a
ian898 <ianblockwell@gmail.com>
2016-11-11 13:55:39 +0000

    Update ssWeatherForecast.lua
    Fixed WeatherForecast Position.

--- src/ssWeatherForecast.lua
+++ src/ssWeatherForecast.lua
@@ -5,21 +5,21 @@
 -- Authors:  Authors:  Akuenzi, ian898, Jarvixes, theSeb
 --
 
-ssWeatherForecast = {}
-ssWeatherForecast.forecast = {} --day of week, low temp, high temp, weather condition
+ssWeatherForecast = {};
+ssWeatherForecast.forecast = {}; --day of week, low temp, high temp, weather condition
 ssWeatherForecast.forecastLength = 7;
 ssWeatherForecast.lastForecastPrediction = 0;
 ssWeatherForecast.modDirectory = g_currentModDirectory;
 
 function ssWeatherForecast:loadMap(name)
-    print("ssWeatherForecast mod loading")
-    g_currentMission.ssWeatherForecast = self
+    print("ssWeatherForecast mod loading");
+    g_currentMission.ssWeatherForecast = self;
     
 	self.hud = {};
     self.hud.visible = true;
 	
-	self.hud.posY = -0.123;
-	self.hud.posX = -0.310;
+	self.hud.posY = -0.14;
+	self.hud.posX = 0.504;
 	
 	self.hud.overlays = {};
 	
@@ -48,13 +48,13 @@ function ssWeatherForecast:loadMap(name)
     self.hud.overlays.weather_hail = g_currentMission.weatherForecastIconOverlays.hail;
 	self.hud.overlays.weather_snow = Overlay:new("hud_snow", Utils.getFilename("huds/hud_snow.png", self.modDirectory), 0, 0, width, height);
 	
-end
+end;
 
 function ssWeatherForecast:deleteMap()
-end
+end;
 
 function ssWeatherForecast:mouseEvent(posX, posY, isDown, isUp, button)
-end
+end;
 
 function ssWeatherForecast:keyEvent(unicode, sym, modifier, isDown)
     if (unicode == 107) then --TODO: this will need to be changed to use a proper inputbinding. Seb: I still
@@ -104,9 +104,9 @@ function ssWeatherForecast:keyEvent(unicode, sym, modifier, isDown)
         -- end
 
         if(self.hud.visible == false) then
-            self.hud.visible = true
+            self.hud.visible = true;
         else
-            self.hud.visible = false
+            self.hud.visible = false;
         end
     end
 end
@@ -114,25 +114,25 @@ end
 function ssWeatherForecast:update(dt)
     -- Predict the weather once a day, for a whole week
     -- FIXME(jos): is this the best solution? How about weather over a long period of time, like, one season? Or a year?
-    local today = g_currentMission.ssSeasonsUtil:currentDayNumber()
+    local today = g_currentMission.ssSeasonsUtil:currentDayNumber();
     if (self.lastForecastPrediction < today) then
-        self:buildForecast()
-        self.lastForecastPrediction = today
+        self:buildForecast();
+        self.lastForecastPrediction = today;
     end
 end
 
 function ssWeatherForecast:draw()
-    local todaysWeather
-    local tomorrowsWeather = self.forecast[1].weatherState .. " forecast tomorrow day num: " .. tostring(self.forecast[1].day)
-    local textToDisplay = "Next day weather: " .. tomorrowsWeather
+    local todaysWeather;
+    local tomorrowsWeather = self.forecast[1].weatherState .. " forecast tomorrow day num: " .. tostring(self.forecast[1].day);
+    local textToDisplay = "Next day weather: " .. tomorrowsWeather;
 
-    renderText(0.25, 0.98, 0.01, textToDisplay)
+    renderText(0.25, 0.98, 0.01, textToDisplay);
 
     if self.hud.visible then
 		
-		-- Get timeBgOverlay position offset WeatherForecast overlay.
-		local WeatherForecastPosX = g_currentMission.timeBgOverlay.x + self.hud.posX;
-		local WeatherForecastPosY = g_currentMission.timeBgOverlay.y + self.hud.posY;
+		-- Set position WeatherForecast overlay.
+		local WeatherForecastPosX = self.hud.posX;
+		local WeatherForecastPosY = g_currentMission.moneyIconOverlay.y + self.hud.posY;
 		
 		-- Render Background
 		renderOverlay(self.hud.overlays.forecast_hud.overlayId, WeatherForecastPosX,  WeatherForecastPosY, 0.52, 0.114);
@@ -165,31 +165,30 @@ function ssWeatherForecast:draw()
 		
 		-- Clean up after us, text render after this will be affected otherwise.
 		setTextColor(1, 1, 1, 1);
-		setTextAlignment(RenderText.ALIGN_LEFT);		
 		
     end;
 	
 end;
 
 function ssWeatherForecast:buildForecast()
-    local startDayNum = g_currentMission.ssSeasonsUtil:currentDayNumber()
-    log("Building forecast based on today day num: " .. startDayNum)
+    local startDayNum = g_currentMission.ssSeasonsUtil:currentDayNumber();
+    log("Building forecast based on today day num: " .. startDayNum);
 
     -- Empty the table
-    self.forecast = {}	
+    self.forecast = {};
 
     for n = 1, self.forecastLength do
-        local oneDayForecast = {}
+        local oneDayForecast = {};
 
-        oneDayForecast.day = startDayNum + n -- To match forecast with actual game
-        oneDayForecast.weekDay =  g_currentMission.ssSeasonsUtil.weekDays[g_currentMission.ssSeasonsUtil:dayOfWeek(startDayNum + n)]
+        oneDayForecast.day = startDayNum + n; -- To match forecast with actual game
+        oneDayForecast.weekDay =  g_currentMission.ssSeasonsUtil.weekDays[g_currentMission.ssSeasonsUtil:dayOfWeek(startDayNum + n)];
 
-        oneDayForecast.lowTemp = g_currentMission.environment.weatherTemperaturesNight[n+1]
-        oneDayForecast.highTemp = g_currentMission.environment.weatherTemperaturesDay[n+1]
+        oneDayForecast.lowTemp = g_currentMission.environment.weatherTemperaturesNight[n+1];
+        oneDayForecast.highTemp = g_currentMission.environment.weatherTemperaturesDay[n+1];
 
-        oneDayForecast.weatherState = self:getWeatherStateForDay(startDayNum + n)
+        oneDayForecast.weatherState = self:getWeatherStateForDay(startDayNum + n);
 
-        table.insert(self.forecast, oneDayForecast)
+        table.insert(self.forecast, oneDayForecast);
     end
 
     --now we check through the rains table to find bad weather
