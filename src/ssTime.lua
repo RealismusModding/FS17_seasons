8ae75aa1294e72049be09e51a661017b060ca0ab
Jos Kuijpers <jkuijpers@jarvix.nl>
2016-11-15 23:29:57 +0100

    Redo the changes of curves when changing day/night time

--- src/ssTime.lua
+++ src/ssTime.lua
@@ -8,20 +8,18 @@
 ssTime = {}
 
 function ssTime:loadMap(name)
-    self:copyDefaultKeyframes()
-
     -- Calculate some constants for the daytime calculator
     local L = 51.9 -- FIXME(jos): Get from savegame
     self.sunRad = L * math.pi / 180
     self.pNight = 6 * math.pi / 180 -- Suns inclination below the horizon for 'civil twilight'
-    self.pDay = -1 * math.pi / 180 -- Suns inclination above the horizon for 'daylight' assumed to be one degree above horizon
+    self.pDay = -10 * math.pi / 180 -- Suns inclination above the horizon for 'daylight' assumed to be one degree above horizon
 
     -- Update time before game start to prevent sudden change of darkness
     self:adaptTime()
 
     g_currentMission.environment:addDayChangeListener(self);
 
-    -- g_currentMission.missionInfo.timeScale = 120*6
+    g_currentMission.missionInfo.timeScale = 120*6*2
 end
 
 function ssTime:deleteMap()
@@ -48,34 +46,45 @@ end
 
 -- Change the night/day times according to season
 function ssTime:adaptTime()
-    -- This is for showing the red-sky texture (I think). Jos: nope it does not
-    -- g_currentMission.environment.skyDayTimeStart = (endTime + morningSkyAdj) * 60 * 1000 -- 3
-    -- g_currentMission.environment.skyDayTimeEnd = (startTime + eveningSkyAdj) * 60 * 1000 --17
-
-    log("------------------")
+    local env = g_currentMission.environment;
 
     -- All local values are in minutes
-    local startTime, endTime = self:calculateStartEndOfDay(ssSeasonsUtil:currentDayNumber())
-    -- local startTime, endTime = self:calculateStartEndOfDay(35) -- 15 = midsummer, 35 = midwinter
+    local dayStart, dayEnd, nightEnd, nightStart = self:calculateStartEndOfDay(ssSeasonsUtil:currentDayNumber())
+    -- local dayStart, dayEnd, nightEnd, nightStart = self:calculateStartEndOfDay(35) -- 15 = midsummer, 35 = midwinter
 
-    log(string.format("day %f -> %f", startTime/60, endTime/60))
+    -- Restrict the values to prevent errors
+    nightEnd = math.max(nightEnd, 1.01) -- nightEnd > 1.0
+    dayStart = math.min(dayStart, 11.99) -- dayStart < 12
+    dayEnd = math.max(dayEnd, 12.01) -- dayEnd > 12
+    nightStart = math.min(nightStart, 22.99) -- nightStart < 23
+
+    -- GIANTS values:
+    -- nightEnd: 4
+    -- dayStart: 9
+    -- dayEnd: 17
+    -- nightStart: 22
+
+    log(string.format("-- night --> %f -> %f -- day --> %f -> %f -- night -- >", nightEnd, dayStart, dayEnd, nightStart))
 
     -- This is for the logical night. Used for turning on lights in houses / streets. Might need some more adjustment.
     -- FIXME(jos): Maybe turn them on between the beginOfNight and fullNight?
-    g_currentMission.environment.nightStart = startTime
-    g_currentMission.environment.nightEnd = endTime
+    env.nightStart = dayStart * 60
+    env.nightEnd = dayEnd * 60
+
+    -- FIXME what is this for?
+    env.skyDayTimeStart = nightEnd * 60 * 60 * 1000
+    env.skyDayTimeEnd = dayEnd * 60 * 60 * 1000
 
     -- For the visual looks
-    g_currentMission.environment.skyCurve.keyframes = self:compressedNightKeyframes(self.skyCurveOriginal, endTime, startTime)
-    g_currentMission.environment.ambientCurve.keyframes = self:compressedNightKeyframes(self.ambientCurveOriginal, endTime, startTime)
-    -- g_currentMission.environment.sunRotCurve.keyframes = self:compressedNightKeyframes(self.sunRotOriginal, endTime, startTime)
-    g_currentMission.environment.sunColorCurve.keyframes = self:compressedNightKeyframes(self.sunColorCurveOriginal, endTime, startTime)
-
-    log("------------------")
+    env.skyCurve = self:generateSkyCurve(nightEnd, dayStart, dayEnd, nightStart)
+    env.ambientCurve = self:generateAmbientCurve(nightEnd, dayStart, dayEnd, nightStart)
+    env.sunRotCurve = self:generateSunRotCurve(nightEnd, dayStart, dayEnd, nightStart)
+    env.sunColorCurve = self:generateSunColorCurve(nightEnd, dayStart, dayEnd, nightStart)
 
     self.lastUpdate = ssSeasonsUtil:currentDayNumber()
 end
 
+-- Output in hours
 function ssTime:calculateStartEndOfDay(dayNumber)
     local dayStart, dayEnd, julianDay, theta, eta
 
@@ -89,9 +98,9 @@ function ssTime:calculateStartEndOfDay(dayNumber)
     dayStart, dayEnd = self:_calculateDay(self.pDay, eta, julianDay)
 
     -- True blackness
-    -- nightStart, nightEnd = self:_calculateDay(self.pNight, eta, julianDay)
+    nightStart, nightEnd = self:_calculateDay(self.pNight, eta, julianDay)
 
-    return dayStart * 60, dayEnd * 60
+    return dayStart, dayEnd, nightStart, nightEnd
 end
 
 function ssTime:_calculateDay(p, eta, julianDay)
@@ -109,8 +118,9 @@ function ssTime:_calculateDay(p, eta, julianDay)
     end
 
     -- Daylight saving between 1 April and 31 October as an approcimation
-    local hasDST = not ((julianDay < 91 or julianDay > 304) or ((julianDay >= 91 and julianDay <= 304) and (gamma < -1 or gamma > 1)))
-    offset = hasDST and 1 or 0
+    -- local hasDST = not ((julianDay < 91 or julianDay > 304) or ((julianDay >= 91 and julianDay <= 304) and (gamma < -1 or gamma > 1)))
+    -- offset = hasDST and 1 or 0
+    offset = 1 -- always keep winter time (no summer time)
 
     timeStart = 12 - D / 2 + offset
     timeEnd = 12 + D / 2 + offset
@@ -118,68 +128,105 @@ function ssTime:_calculateDay(p, eta, julianDay)
     return timeStart, timeEnd
 end
 
-function ssTime:copyDefaultKeyframes()
-    self.skyCurveOriginal = deepCopy(g_currentMission.environment.skyCurve.keyframes)
-    self.ambientCurveOriginal = deepCopy(g_currentMission.environment.ambientCurve.keyframes)
-    self.sunRotOriginal = deepCopy(g_currentMission.environment.sunRotCurve.keyframes)
-    self.sunColorCurveOriginal = deepCopy(g_currentMission.environment.sunColorCurve.keyframes)
+function ssTime:generateAmbientCurve(nightEnd, dayStart, dayEnd, nightStart)
+    local curve = AnimCurve:new(linearInterpolator3) -- degree 2
+
+    local morningStep = (dayStart - nightEnd) / 5
+    local eveningStep = (nightStart - dayEnd) / 5
+
+    curve:addKeyframe({x = 0.020, y = 0.020, z = 0.032, time = 0.00 * 60})
+    curve:addKeyframe({x = 0.020, y = 0.020, z = 0.032, time = (nightEnd - 0.25 * morningStep) * 60})
+    curve:addKeyframe({x = 0.065, y = 0.065, z = 0.075, time = (nightEnd + 0.01) * 60}) -- light up ambient to compensate for switch to sunlight
+    curve:addKeyframe({x = 0.065, y = 0.065, z = 0.075, time = (nightEnd + 0.02) * 60})
+    curve:addKeyframe({x = 0.040, y = 0.040, z = 0.060, time = (nightEnd + 0.25 * morningStep) * 60}) -- back to regular ambient settings
+    curve:addKeyframe({x = 0.080, y = 0.080, z = 0.140, time = (nightEnd + 1 * morningStep) * 60})
+    curve:addKeyframe({x = 0.140, y = 0.140, z = 0.120, time = (nightEnd + 2 * morningStep) * 60})
+    curve:addKeyframe({x = 0.200, y = 0.200, z = 0.200, time = dayStart * 60})
+
+    curve:addKeyframe({x = 0.200, y = 0.200, z = 0.200, time = dayEnd * 60})
+    curve:addKeyframe({x = 0.160, y = 0.140, z = 0.160, time = (dayEnd + 1 * eveningStep) * 60})
+    curve:addKeyframe({x = 0.020, y = 0.020, z = 0.032, time = (dayEnd + 4 * eveningStep) * 60}) -- ambient night
+    curve:addKeyframe({x = 0.024, y = 0.024, z = 0.036, time = (nightStart - 0.15 * eveningStep) * 60})
+    curve:addKeyframe({x = 0.072, y = 0.072, z = 0.088, time = (nightStart + 0.01) * 60}) -- light up ambient to compensate for switch to moonlight
+    curve:addKeyframe({x = 0.072, y = 0.072, z = 0.088, time = (nightStart + 0.02) * 60})
+    curve:addKeyframe({x = 0.020, y = 0.020, z = 0.032, time = (nightStart + 0.15 * eveningStep) * 60}) -- back to regular ambient settings
+    curve:addKeyframe({x = 0.020, y = 0.020, z = 0.032, time = 24.00 * 60})
+
+    return curve;
 end
 
-function ssTime:compressedNightKeyframes(keyframes, beginNight, endNight)
-    local newFrames = deepCopy(keyframes)
-    local numFrames = arrayLength(keyframes)
-    local oldNightEnd, oldNightBegin
-    local pivot, pivotGap = nil, 0
+function ssTime:generateSunColorCurve(nightEnd, dayStart, dayEnd, nightStart)
+    local curve = AnimCurve:new(linearInterpolator3) -- degree 2
 
-    -- Verify first frame = time 0, last = time 1440
-    -- FIXME: move this all to somewhere else, it all needs to be done just once.
-    if keyframes[1].time ~= 0 or keyframes[numFrames].time ~= 1440 then
-        -- Todo: make this a UTIL
-        logInfo("Could not change keyframes due to unknown keyframes format")
-        return
-    end
+    local morningStep = (dayStart - nightEnd) / 5
+    local eveningStep = (nightStart - dayEnd) / 5
 
-    -- Find the pivot (center) by looking between 5:00 and 21:00 for the biggest gap
-    for i = 1, numFrames do
-        if keyframes[i].time >= 5 * 60 and keyframes[i].time <= 21 * 60 then
-            local newGap = keyframes[i + 1].time - keyframes[i].time
+    curve:addKeyframe({x = 0.060, y = 0.070, z = 0.150, time = 0.00 * 60})
+    curve:addKeyframe({x = 0.060, y = 0.070, z = 0.150, time = (nightEnd - 0.25 * morningStep) * 60})
+    curve:addKeyframe({x = 0.000, y = 0.000, z = 0.000, time = (nightEnd + 0.01) * 60}) -- shift to brief complete darkness for switch to sunlight
+    curve:addKeyframe({x = 0.000, y = 0.000, z = 0.000, time = (nightEnd + 0.02) * 60})
+    curve:addKeyframe({x = 0.060, y = 0.050, z = 0.030, time = (nightEnd + 0.25 * morningStep) * 60})
+    curve:addKeyframe({x = 0.700, y = 0.450, z = 0.350, time = (nightEnd + 2 * morningStep) * 60}) -- lighten, red
+    curve:addKeyframe({x = 1.100, y = 0.850, z = 0.600, time = (nightEnd + 3 * morningStep) * 60}) -- shift to orange
+    curve:addKeyframe({x = 0.750, y = 0.750, z = 0.750, time = dayStart * 60})
 
-            if newGap > pivotGap then
-                pivotGap = newGap
-                pivot = i
-            end
-        end
-    end
+    curve:addKeyframe({x = 0.750, y = 0.750, z = 0.750, time = dayEnd * 60})
+    curve:addKeyframe({x = 0.750, y = 0.700, z = 0.650, time = (dayEnd + 1 * eveningStep) * 60}) -- darken
+    curve:addKeyframe({x = 1.000, y = 0.900, z = 0.300, time = (dayEnd + 2 * eveningStep) * 60}) -- orange sunset
+    curve:addKeyframe({x = 0.900, y = 0.400, z = 0.100, time = (dayEnd + 3 * eveningStep) * 60}) -- shift to red
+    curve:addKeyframe({x = 0.300, y = 0.250, z = 0.500, time = (dayEnd + 4 * eveningStep) * 60}) -- shift to blue
+    curve:addKeyframe({x = 0.050, y = 0.060, z = 0.100, time = (nightStart - 0.15 * eveningStep) * 60})
+    curve:addKeyframe({x = 0.000, y = 0.000, z = 0.000, time = (nightStart + 0.01) * 60}) -- shift to brief complete darkness for switch to moonlight
+    curve:addKeyframe({x = 0.000, y = 0.000, z = 0.000, time = (nightStart + 0.02) * 60})
+    curve:addKeyframe({x = 0.050, y = 0.060, z = 0.100, time = (nightStart + 0.15 * eveningStep) * 60})
+    curve:addKeyframe({x = 0.060, y = 0.070, z = 0.150, time = 24.00 * 60}) -- shift to bluish darkness
 
-    if not pivot then
-        logInfo("Could not find pivot")
-        return
-    end
-    -- END OF FIXME
-
-    log("Found pivot at "..tostring(pivot).." with gap "..tostring(pivotGap))
-
-    -- Find old begin and end time of night
-    oldNightEnd = keyframes[pivot].time -- in minutes (early)
-    oldNightBegin = keyframes[pivot + 1].time -- in minutes (late)
-
-    -- Calculate compression rate for early and late
-    local compressionEarly = endNight / oldNightEnd
-    local compressionLate = (1440 - beginNight) / (1440 - oldNightBegin)
-
-    log("compression early "..tostring(compressionEarly)..", late "..tostring(compressionLate))
-
-    -- Rewrite times for early and late
-    for i = 1, numFrames do
-        if i <= pivot then -- early
-            newFrames[i].time = keyframes[i].time * compressionEarly
-        else -- late
-            newFrames[i].time = 1440 - ((1440 - keyframes[i].time) * compressionLate)
-        end
-    end
-
-    return newFrames
+    return curve
 end
+
+function ssTime:generateSkyCurve(nightEnd, dayStart, dayEnd, nightStart)
+    local curve = AnimCurve:new(linearInterpolator4) -- degree 2
+
+    local morningStep = (dayStart - nightEnd) / 10
+    local eveningStep = (nightStart - dayEnd) / 5
+
+    -- The sum of the 4 elements should always be 1
+    -- x = daySky, y = eveningSky, z = nightSky, w = morningSky
+    -- 4.5 -> 5.5 -> 6.0 = night morning transition
+    -- 6.0 -> 7.0 -> 8.0 = morning day transition
+    curve:addKeyframe({x = 0.0, y = 0.0, z = 1.0, w = 0.0, time = 0.0 * 60}) -- night
+    curve:addKeyframe({x = 0.0, y = 0.0, z = 1.0, w = 0.0, time = (nightEnd + 1 * morningStep) * 60}) -- night
+    curve:addKeyframe({x = 0.0, y = 0.0, z = 0.5, w = 0.5, time = (nightEnd + 3 * morningStep) * 60}) -- night/morning
+    curve:addKeyframe({x = 0.0, y = 0.0, z = 0.0, w = 1.0, time = (nightEnd + 4 * morningStep) * 60}) -- morning
+    curve:addKeyframe({x = 0.5, y = 0.0, z = 0.0, w = 0.5, time = (nightEnd + 6 * morningStep) * 60}) -- morning/day
+    curve:addKeyframe({x = 1.0, y = 0.0, z = 0.0, w = 0.0, time = (nightEnd + 8 * morningStep) * 60}) -- day
+
+    curve:addKeyframe({x = 1.0, y = 0.0, z = 0.0, w = 0.0, time = dayEnd * 60}) -- day
+    curve:addKeyframe({x = 0.0, y = 1.0, z = 0.0, w = 0.0, time = (dayEnd + 2 * eveningStep) * 60}) -- evening
+    curve:addKeyframe({x = 0.0, y = 1.0, z = 0.0, w = 0.0, time = (dayEnd + 3 * eveningStep) * 60}) -- evening
+    curve:addKeyframe({x = 0.0, y = 0.0, z = 1.0, w = 0.0, time = nightStart * 60}) -- night
+    curve:addKeyframe({x = 0.0, y = 0.0, z = 1.0, w = 0.0, time = 24.0 * 60}) -- night
+
+    return curve
+end
+
+function ssTime:generateSunRotCurve(nightEnd, dayStart, dayEnd, nightStart)
+    local curve = AnimCurve:new(linearInterpolator1) -- degree 2
+
+    curve:addKeyframe({v = Utils.degToRad(-15), time = 0.00 * 60}) -- start (moon)
+    curve:addKeyframe({v = Utils.degToRad(  0), time = 1.00 * 60}) -- 15 per hour
+    curve:addKeyframe({v = Utils.degToRad( 45), time = nightEnd * 60}) -- 15 per hour
+    curve:addKeyframe({v = Utils.degToRad( 45), time = (nightEnd + 0.01) * 60})
+    curve:addKeyframe({v = Utils.degToRad(-70), time = (nightEnd + 0.02) * 60}) -- switch to Sun
+    curve:addKeyframe({v = Utils.degToRad(  0), time = 12.00 * 60}) -- rotate over the day
+    curve:addKeyframe({v = Utils.degToRad( 70), time = (nightStart + 0.01) * 60}) -- end rotation of sun
+    curve:addKeyframe({v = Utils.degToRad(-35), time = (nightStart + 0.02) * 60}) -- switch to moon light
+    curve:addKeyframe({v = Utils.degToRad(-15), time = 24.00 * 60}) -- 10 per hour
+
+    return curve
+end
+
+-- dustDensity
 
 function ssTime:dayChanged()
     -- Update the time of the day
