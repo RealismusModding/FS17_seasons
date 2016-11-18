0b9df2ef2b2e44e27e4602ddfde7215f05353170
Jos Kuijpers <jkuijpers@jarvix.nl>
2016-11-18 14:47:49 +0100

    Fix maintenance, use standard economy to deduct and reset daily. Update readme

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ This mod inplements the following:
 - seasonal crop growth. You can't grow certain crops when it is too cold. Some crops might even die in the winter. Growth speed of the crop is also decided by the the tenperature and weather
 - 7 day weather forecast with temperatures, season and weather
 - updates to the economy to make this all survivable: realistic income (crops) and expenses (workers, maintenance)
+
+Maintenance
+- Age in maintenance GUI is now days since last repair
+- New maintenance algorithm with repair, maintenance and taxes
+- Keep your machine clean to lower daily maintenance costs
+- Machine not used? Pay less
--- src/ssMaintenance.lua
+++ src/ssMaintenance.lua
@@ -50,7 +50,7 @@ function ssMaintenance:update(dt)
 end
 
 function ssMaintenance:dayChanged()
-    self:deductVehicleCosts()
+    self:resetOperatingTimeAndDirt()
 end
 
 function ssMaintenance:installRepairableSpecialization()
@@ -131,12 +131,6 @@ function ssMaintenance:getDailyUpKeep(superFunc)
         return superFunc(self)
     end
 
-    -- Do 0 when the game system applies the prices. We want to do it ourselves
-    -- so the standard economy system must not deduct anything for us
-    if g_currentMission.environment.dayTime < 2 * 1000 * 60 then
-        return 0
-    end
-
     -- This is for visually in the display
     local costs = ssMaintenance:taxInterestCost(self, storeItem)
     costs = costs + ssMaintenance:maintenanceCost(self, storeItem)
@@ -158,6 +152,7 @@ end
 --     return string.format(g_i18n:getText("shop_maintenanceValue"), g_i18n:formatMoney(dailyUpkeep, 2))
 -- end
 
+-- Replace the age with the age since last repair, because actual age is useless
 function ssMaintenance:getSpecValueAge(superFunc, vehicle)
     if vehicle ~= nil and vehicle.ssLastRepairDay ~= nil then
         return string.format(g_i18n:getText("shop_age"), ssSeasonsUtil:currentDayNumber() - vehicle.ssLastRepairDay)
@@ -168,9 +163,16 @@ function ssMaintenance:getSpecValueAge(superFunc, vehicle)
     return nil
 end
 
-function ssMaintenance:deductVehicleCosts()
-    -- if not SpecializationUtil.hasSpecialization(ssRepairable, self.specializations) then
-    --[[
+function ssMaintenance:resetOperatingTimeAndDirt()
+    for i, vehicle in pairs(g_currentMission.vehicles) do
+        if SpecializationUtil.hasSpecialization(ssRepairable, vehicle.specializations) then
+            vehicle.ssCumulativeDirt = 0
+            vehicle.ssYesterdayOperatingTime = vehicle.operatingTime
+        end
+    end
+end
+
+--[[
     print_r(storeItem)
 
     storeItem.lifetime
@@ -191,26 +193,8 @@ function ssMaintenance:deductVehicleCosts()
     [configFileName] => "data/vehicles/tools/kuhn/kuhnCultimerL300.xml"
     [lastMoveTime] => 16.617000579834
     [dirtAmount] => 0.11339925229549 -- might only be available on "washable", dont use, use the function on the specification
-    ]]
 
-    local totalTaxes = 0
-    local totalMaintenance = 0
-
-    for i, vehicle in pairs(g_currentMission.vehicles) do
-        if SpecializationUtil.hasSpecialization(ssRepairable, vehicle.specializations) then
-            local storeItem = StoreItemsUtil.storeItemsByXMLFilename[vehicle.configFileName:lower()]
-
-            totalTaxes = totalTaxes + self:taxInterestCost(vehicle, storeItem)
-            totalMaintenance = totalMaintenance + self:maintenanceCost(vehicle, storeItem)
-
-            vehicle.ssCumulativeDirt = 0
-            vehicle.ssYesterdayOperatingTime = vehicle.operatingTime
-        end
-    end
-
-    -- Deduct
-    totalMaintenance = totalMaintenance + 1000
-    if g_currentMission:getIsServer() then
+if g_currentMission:getIsServer() then
         g_currentMission:addSharedMoney(-totalTaxes, "vehicleRunningCost")
         g_currentMission:addSharedMoney(-totalMaintenance, "vehicleRunningCost")
 
@@ -222,20 +206,4 @@ function ssMaintenance:deductVehicleCosts()
         g_client:getServerConnection():sendEvent(CheatMoneyEvent:new(-totalTaxes))
         g_client:getServerConnection():sendEvent(CheatMoneyEvent:new(-totalMaintenance))
     end
-end
-
---[[
-local storeItem = StoreItemsUtil.storeItemsByXMLFilename[self.configFileName:lower()]
-4134
-4135        local multiplier = 1
-4136        if storeItem.lifetime ~= nil and storeItem.lifetime ~= 0 then
-4137            local ageMultiplier = 0.3 * math.min(self.age/storeItem.lifetime, 1)
-4138            local operatingTime = self.operatingTime / (1000*60*60)
-4139            local operatingTimeMultiplier =  0.7 * math.min(operatingTime / (storeItem.lifetime*EconomyManager.LIFETIME_OPERATINGTIME_RATIO), 1)
-4140            multiplier = 1 + EconomyManager.MAX_DAILYUPKEEP_MULTIPLIER * (ageMultiplier+operatingTimeMultiplier)
-4141        end
-4142
-4143        return StoreItemsUtil.getDailyUpkeep(storeItem, self.configurations) * multiplier
-4144
 ]]
-
