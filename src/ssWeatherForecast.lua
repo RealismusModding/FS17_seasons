5b487bb3c823c266d5a15d1cc11c2b5648379e93
reallogger <per_moslet@hotmail.com>
2016-11-25 00:04:09 +0100

    Update of temperatures and rain table

--- src/ssWeatherForecast.lua
+++ src/ssWeatherForecast.lua
@@ -19,7 +19,7 @@ end
 
 function ssWeatherForecast:loadMap(name)
     g_currentMission.environment:addDayChangeListener(self);
-    self:buildFirstForecast(); -- Should be read from savegame
+    self:buildForecast(); -- Should be read from savegame
 
     self.hud = {};
     self.hud.visible = true;
@@ -127,97 +127,55 @@ function ssWeatherForecast:draw()
 
 end
 
-function ssWeatherForecast:buildFirstForecast()
+function ssWeatherForecast:buildForecast()
     local startDayNum = ssSeasonsUtil:currentDayNumber();
     local ssTmax
-    log("Building forecast based on today day num: " .. startDayNum);
-
-    self.forecast = {};
+	log("Building forecast based on today day num: " .. startDayNum);
+		
+	self.forecast = {};
 
     for n = 1, self.forecastLength do
         local oneDayForecast = {};
+	    local ssTmax = {};
+	    local Tmaxmean = {};
 
-        oneDayForecast.day = startDayNum + n; -- To match forecast with actual game
+		oneDayForecast.day = startDayNum + n; -- To match forecast with actual game
         oneDayForecast.weekDay =  ssSeasonsUtil:dayName(startDayNum + n);
-        oneDayForecast.season = ssSeasonsUtil:seasonName(startDayNum + n)
-
-        if n == 1 then
-            --Seasonal average for first day
-            ssTmax = self:ssTmax(oneDayForecast.season)
-            oneDayForecast.Tmaxmean = ssSeasonsUtil:ssTriDist(ssTmax)
-
-        elseif self.forecast[n-1].season == oneDayForecast.season then
-            --Seasonal average for a day in the season
-            oneDayForecast.Tmaxmean = self.forecast[n-1].Tmaxmean
-
-        elseif self.forecast[n-1].season ~= oneDayForecast.season then
-            --Seasonal average for a day in a new season
-            ssTmax = self:ssTmax(oneDayForecast.season)
-            oneDayForecast.Tmaxmean = ssSeasonsUtil:ssTriDist(ssTmax)
-
-        end
-
-        oneDayForecast.highTemp = ssSeasonsUtil:ssNormDist(oneDayForecast.Tmaxmean,2.5)
-        oneDayForecast.lowTemp = ssSeasonsUtil:ssNormDist(0,2) + 0.75 * oneDayForecast.highTemp-5
-
+		oneDayForecast.season = ssSeasonsUtil:seasonName(startDayNum + n)
+		
+		ssTmax = self:Tmax(oneDayForecast.season)
+        log('ssTmax = ',ssTmax[2])
+		oneDayForecast.highTemp = ssSeasonsUtil:ssNormDist(ssTmax[2],2.5) 
+		oneDayForecast.lowTemp = ssSeasonsUtil:ssNormDist(0,2) + 0.75 * ssTmax[2]-5
+		
         oneDayForecast.weatherState = self:getWeatherStateForDay(startDayNum + n);
 
         table.insert(self.forecast, oneDayForecast);
     end
 
-    --now we check through the rains table to find bad weather
-    -- for index, rain in ipairs(g_currentMission.environment.rains) do
-    --     log("Bad weather predicted for day: " .. tostring(rain.startDay) .. " weather type: " .. rain.rainTypeId .. " index: " .. tostring(index))
-    --     if rain.startDay > self.forecastLength+1 then
-    --         break
-    --     end
-    --     foreCastDayIndex = rain.startDay -1
-    --     self.forecast[foreCastDayIndex].weatherState = rain.rainTypeId
-    -- end
-
-end
-
-function ssWeatherForecast:buildForecast()
-    local startDayNum = ssSeasonsUtil:currentDayNumber();
-    log("Building forecast based on today day num: " .. startDayNum);
-
-    table.remove(self.forecast,1)
-
-    local oneDayForecast = {};
-    oneDayForecast.day = startDayNum + self.forecastLength; -- To match forecast with actual game
-    oneDayForecast.weekDay =  ssSeasonsUtil:dayName(startDayNum + self.forecastLength);
-    oneDayForecast.season = ssSeasonsUtil:seasonName(startDayNum + self.forecastLength)
-
-    if self.forecast[self.forecastLength-1].season == oneDayForecast.season then
-        --Seasonal average for a day in the season
-        oneDayForecast.Tmaxmean = self.forecast[self.forecastLength-1].Tmaxmean
-
-    elseif self.forecast[self.forecastLength-1].season ~= oneDayForecast.season then
-        --Seasonal average for a day in a new season
-        ssTmax = self:ssTmax(oneDayForecast.season)
-        oneDayForecast.Tmaxmean = ssSeasonsUtil:ssTriDist(ssTmax)
-
+    
+    print_r(self.forecast)
+	for index, rain in ipairs(g_currentMission.environment.rains) do
+        for jndex, fCast in ipairs(self.forecast) do
+             if (rain.startDay == fCast.day) then
+                log('fCast.day = ', fCast.day,'is equal to ', rain.startDay)
+                log('oneDayForecast = ',oneDayForecast)
+                log('rain.rainTypeId = ', rain.rainTypeId)
+                if fCast.lowTemp < -1 and rain.rainTypeId == 'rain' then
+				    g_currentMission.environment.rains[index].rainTypeId = 'hail'
+                    self.forecast[jndex].weatherState = self:getWeatherStateForDay(fCast.day);
+			    elseif fCast.lowTemp >= -1 and rain.rainTypeId == 'hail' then
+				    g_currentMission.environment.rains[index].rainTypeId = 'rain'
+                    oneDayForecast.weatherState = self:getWeatherStateForDay(fCast.day);
+			    end
+            end
+        end
     end
 
-    oneDayForecast.highTemp = ssSeasonsUtil:ssNormDist(oneDayForecast.Tmaxmean,2.5)
-    oneDayForecast.lowTemp = ssSeasonsUtil:ssNormDist(0,2) + 0.75 * oneDayForecast.highTemp-5
-
-    oneDayForecast.weatherState = self:getWeatherStateForDay(startDayNum);
-
-    table.insert(self.forecast, oneDayForecast);
-
-
-    --now we check through the rains table to find bad weather
-    -- for index, rain in ipairs(g_currentMission.environment.rains) do
-    --     log("Bad weather predicted for day: " .. tostring(rain.startDay) .. " weather type: " .. rain.rainTypeId .. " index: " .. tostring(index))
-    --     if rain.startDay > self.forecastLength+1 then
-    --         break
-    --     end
-    --     foreCastDayIndex = rain.startDay -1
-    --     self.forecast[foreCastDayIndex].weatherState = rain.rainTypeId
-    -- end
+    print_r(g_currentMission.environment.rains)
 
 end
+
 
 -- FIXME: not the best to be iterating within another loop, but since we are only doing this once a day, not a massive issue
 --perhaps rewrite so that initial forecast is generated for 7 days and then next day only remove the first element and add the next day?
@@ -247,10 +205,11 @@ function ssWeatherForecast:dayChanged()
     self:buildForecast();
 end
 
-function ssWeatherForecast:ssTmax(ss) --sets the minimum, mode and maximum of the seasonal average maximum temperature. Simplification due to unphysical bounds.
+function ssWeatherForecast:Tmax(ss) --sets the minimum, mode and maximum of the seasonal average maximum temperature. Simplification due to unphysical bounds.
 
     if ss == 'Winter' then
-        return {5.0,8.6,10.7} --min, mode, max
+        -- return {5.0,8.6,10.7} --min, mode, max Temps from the data
+        return {0.0,3.6,5.7} --min, mode, max
 
     elseif ss == "Spring" then
         return {12.1, 14.2, 17.9} --min, mode, max
@@ -264,5 +223,48 @@ function ssWeatherForecast:ssTmax(ss) --sets the minimum, mode and maximum of th
 
 end
 
+-- function to output the temperature during the day and night
+function ssWeatherForecast:diurnalTemp(hour,minute)
+    -- need to have the high temp of the previous day    
+    -- hour is hour in the day from 0 to 23
+    -- minute is minutes from 0 to 59
 
+    prevDayTemp = self.forecast[1].highTemp -- not completely correct, but instead of storing the temp of the previous day
 
+    local currentTime = hour*60 + minute
+
+    if currentTime < 420 then
+	    currentTemp = ( math.cos(((currentTime + 540)/960)*math.pi/2) )^3 * (preDayTemp-self.forecast[1].lowTemp)+self.forecast[1].lowTemp
+    elseif currentTime > 900 then
+	    currentTemp = ( math.cos(((currentTime - 900)/960)*math.pi/2) )^3 * (self.forecast[1].highTemp-self.forecast[2].lowTemp)+self.forecast[1].lowTemp
+    else
+	    currentTemp = ( math.cos((1- (currentTime-420)/480)*math.pi/2 )^3) * (self.forecast[1].highTemp-self.forecast[1].lowTemp) + self.forecast[1].lowTemp   
+    end
+
+return currentTemp
+
+end
+
+--- function to keep track of snow accumulation
+function ssWeatherForecast:snowAccumulation()
+    currentRain = g_currentMission.environment.currentRain
+    currentTemp = ssWeatherForecast.diurnalTemp(g_currentMission.environment.currentHour,g_currentMission.environment.currentMinute)
+
+    if currentRain == "sun" then
+        if currentTemp > -1  then -- snow melts at -1 if the sun is shining
+            self.snowDepth = self.snowDepth - (-2*currentTemp+7.5)
+        end
+    elseif currentRain == "rain" then
+        -- snow melts twice as fast if it rains
+        self.snowDepth = self.snowDepth - (-2*currentTemp+7.5) * 2
+    elseif currentRain == "hail" then
+        -- Initial value of 10 mm/hr accumulation rate
+        self.snowDepth = self.snowDepth + 10
+    else
+        -- 30% melting when there is cloudy and fog
+        self.snowDepth = self.snowDepth - (-2*currentTemp+7.5)*0.3
+    end
+
+    return math.ceil(self.snowDepth/6/10)
+
+end
