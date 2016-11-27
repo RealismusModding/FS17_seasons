d508668e3ab79b2f8fc084fedbd28801d85d96df
Jos Kuijpers <jkuijpers@jarvix.nl>
2016-11-27 14:54:06 +0100

    Make the WF hud work in MP (presumably)

--- src/ssWeatherForecast.lua
+++ src/ssWeatherForecast.lua
@@ -8,43 +8,36 @@
 ssWeatherForecast = {}
 
 function ssWeatherForecast:loadMap(name)
-    -- g_currentMission.environment:addDayChangeListener(self)
+    if g_currentMission:getIsClient() then
+        self.hud = {}
+        self.hud.visible = true
 
-    self.hud = {}
-    self.hud.visible = true
+        self.hud.posY = -0.14
+        self.hud.posX = 0.504
 
-    self.hud.posY = -0.14
-    self.hud.posX = 0.504
+        self.hud.overlays = {}
 
-    self.hud.overlays = {}
+        -- Forecast hud
+        local width, height = getNormalizedScreenValues(1024, 128)
+        self.hud.overlays.forecast_hud = Overlay:new("hud_forecast", Utils.getFilename("huds/hud_forecast.png", ssSeasonsMod.modDir), 0, 0, width, height)
 
-    -- Forecast hud
-    local width, height = getNormalizedScreenValues(1024, 128)
-    self.hud.overlays.forecast_hud = Overlay:new("hud_forecast", Utils.getFilename("huds/hud_forecast.png", ssSeasonsMod.modDir), 0, 0, width, height)
+        -- Seasons "White" Icons
+        local width, height = getNormalizedScreenValues(128, 128)
 
-    -- Seasons "White" Icons
-    local width, height = getNormalizedScreenValues(128, 128)
-    --self.hud.overlays.season_spring = Overlay:new("hud_spring", Utils.getFilename("huds/hud_spring.png", self.modDirectory), 0, 0, width, height)
-    --self.hud.overlays.season_summer = Overlay:new("hud_summer", Utils.getFilename("huds/hud_summer.png", self.modDirectory), 0, 0, width, height)
-    --self.hud.overlays.season_autum = Overlay:new("hud_autum", Utils.getFilename("huds/hud_autum.png", self.modDirectory), 0, 0, width, height)
-    --self.hud.overlays.season_winter = Overlay:new("hud_winter", Utils.getFilename("huds/hud_winter.png", self.modDirectory), 0, 0, width, height)
+        -- Seasons "Color" Icons
+        self.hud.overlays.Spring = Overlay:new("hud_spring", Utils.getFilename("huds/hud_Season_Color/hud_spring_Color.png", ssSeasonsMod.modDir), 0, 0, width, height)
+        self.hud.overlays.Summer = Overlay:new("hud_summer", Utils.getFilename("huds/hud_Season_Color/hud_summer_Color.png", ssSeasonsMod.modDir), 0, 0, width, height)
+        self.hud.overlays.Autumn = Overlay:new("hud_autum", Utils.getFilename("huds/hud_Season_Color/hud_autum_Color.png", ssSeasonsMod.modDir), 0, 0, width, height)
+        self.hud.overlays.Winter = Overlay:new("hud_winter", Utils.getFilename("huds/hud_Season_Color/hud_winter_Color.png", ssSeasonsMod.modDir), 0, 0, width, height)
 
-    -- Seasons "Color" Icons
-    self.hud.overlays.Spring = Overlay:new("hud_spring", Utils.getFilename("huds/hud_Season_Color/hud_spring_Color.png", ssSeasonsMod.modDir), 0, 0, width, height)
-    self.hud.overlays.Summer = Overlay:new("hud_summer", Utils.getFilename("huds/hud_Season_Color/hud_summer_Color.png", ssSeasonsMod.modDir), 0, 0, width, height)
-    self.hud.overlays.Autumn = Overlay:new("hud_autum", Utils.getFilename("huds/hud_Season_Color/hud_autum_Color.png", ssSeasonsMod.modDir), 0, 0, width, height)
-    self.hud.overlays.Winter = Overlay:new("hud_winter", Utils.getFilename("huds/hud_Season_Color/hud_winter_Color.png", ssSeasonsMod.modDir), 0, 0, width, height)
-
-    -- Seasons Weather Icons
-    self.hud.overlays.sun = g_currentMission.weatherForecastIconSunOverlay
-    self.hud.overlays.cloudy = g_currentMission.weatherForecastIconOverlays.cloudy
-    self.hud.overlays.fog = g_currentMission.weatherForecastIconOverlays.fog
-    self.hud.overlays.rain = g_currentMission.weatherForecastIconOverlays.rain
-    self.hud.overlays.hail = g_currentMission.weatherForecastIconOverlays.hail
-    self.hud.overlays.snow = Overlay:new("hud_snow", Utils.getFilename("huds/hud_snow.png", ssSeasonsMod.modDir), 0, 0, width, height)
-
-    -- g_currentMission.environment.rainFadeDuration = 0--0.5*60000
-    -- g_currentMission.environment.rainFadeDuration.minRainDuration = 60000
+        -- Seasons Weather Icons
+        self.hud.overlays.sun = g_currentMission.weatherForecastIconSunOverlay
+        self.hud.overlays.cloudy = g_currentMission.weatherForecastIconOverlays.cloudy
+        self.hud.overlays.fog = g_currentMission.weatherForecastIconOverlays.fog
+        self.hud.overlays.rain = g_currentMission.weatherForecastIconOverlays.rain
+        self.hud.overlays.hail = g_currentMission.weatherForecastIconOverlays.hail
+        self.hud.overlays.snow = Overlay:new("hud_snow", Utils.getFilename("huds/hud_snow.png", ssSeasonsMod.modDir), 0, 0, width, height)
+    end
 end
 
 function ssWeatherForecast:deleteMap()
@@ -63,14 +56,9 @@ function ssWeatherForecast:update(dt)
 end
 
 function ssWeatherForecast:draw()
+    if not g_currentMission:getIsClient() then return end
+
     local forecast = ssWeatherManager.forecast
-    local forecastLength = ssWeatherManager.forecastLength
-
-    local todaysWeather
-    local tomorrowsWeather = forecast[1].weatherState .. " forecast tomorrow day num: " .. tostring(forecast[1].day)
-    local textToDisplay = "Next day weather: " .. tomorrowsWeather
-
-    renderText(0.25, 0.98, 0.01, textToDisplay)
 
     if self.hud.visible then
         -- Set position WeatherForecast overlay.
@@ -81,13 +69,13 @@ function ssWeatherForecast:draw()
         renderOverlay(self.hud.overlays.forecast_hud.overlayId, WeatherForecastPosX,  WeatherForecastPosY, 0.52, 0.114)
 
         -- Set text color and alignment
-        setTextColor(1,1,1,.9)
+        setTextColor(1, 1, 1, .9)
         setTextAlignment(RenderText.ALIGN_CENTER)
 
         -- Set firstDayPos
         local daysPosOffset = 0.0615
 
-        for n = 1, forecastLength do
+        for n = 1, ssWeatherManager.forecastLength do
             -- Render Day of The Week
             renderText(WeatherForecastPosX + 0.068 + (daysPosOffset * (n - 1)), WeatherForecastPosY + 0.086, 0.02, ssSeasonsUtil:dayNameShort(ssSeasonsUtil:dayOfWeek()+n))
 
@@ -98,7 +86,8 @@ function ssWeatherForecast:draw()
             renderOverlay(self.hud.overlays[forecast[n].weatherState].overlayId, WeatherForecastPosX + 0.053 + (daysPosOffset * (n - 1)), WeatherForecastPosY + 0.026, 0.032, 0.058)
 
             -- Render Hi/Lo Tempratures
-            renderText(WeatherForecastPosX + 0.068 + (daysPosOffset * (n - 1)), WeatherForecastPosY + 0.01, 0.018, tostring(math.floor(forecast[n].highTemp)) .. " / " .. tostring(math.floor(forecast[n].lowTemp)))
+            local tempString = tostring(math.floor(forecast[n].highTemp)) .. " / " .. tostring(math.floor(forecast[n].lowTemp))
+            renderText(WeatherForecastPosX + 0.068 + (daysPosOffset * (n - 1)), WeatherForecastPosY + 0.01, 0.018, tempString)
             --renderText(WeatherForecastPosX + 0.068 + (daysPosOffset * (n - 1)), WeatherForecastPosY + 0.01, 0.018, "22 / 12" )
 
             -- Render Season Days
