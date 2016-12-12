dca0988712da74594966d1b5b2ab6afb69da4677
Jos Kuijpers <jkuijpers@jarvix.nl>
2016-12-12 17:23:31 +0100

    Fix WF when using languages other than english.

--- src/ssWeatherForecast.lua
+++ src/ssWeatherForecast.lua
@@ -35,10 +35,11 @@ function ssWeatherForecast:loadMap(name)
         local width, height = getNormalizedScreenValues(128, 128)
 
         -- Seasons "Color" Icons
-        self.hud.overlays.Spring = Overlay:new("hud_spring", Utils.getFilename("resources/huds/hud_Season_Color/hud_spring_Color.png", ssSeasonsMod.modDir), 0, 0, width, height)
-        self.hud.overlays.Summer = Overlay:new("hud_summer", Utils.getFilename("resources/huds/hud_Season_Color/hud_summer_Color.png", ssSeasonsMod.modDir), 0, 0, width, height)
-        self.hud.overlays.Autumn = Overlay:new("hud_autumn", Utils.getFilename("resources/huds/hud_Season_Color/hud_autumn_Color.png", ssSeasonsMod.modDir), 0, 0, width, height)
-        self.hud.overlays.Winter = Overlay:new("hud_winter", Utils.getFilename("resources/huds/hud_Season_Color/hud_winter_Color.png", ssSeasonsMod.modDir), 0, 0, width, height)
+        self.hud.overlays.seasons = {}
+        self.hud.overlays.seasons[ssSeasonsUtil.SEASON_SPRING] = Overlay:new("hud_spring", Utils.getFilename("resources/huds/hud_Season_Color/hud_spring_Color.png", ssSeasonsMod.modDir), 0, 0, width, height)
+        self.hud.overlays.seasons[ssSeasonsUtil.SEASON_SUMMER] = Overlay:new("hud_summer", Utils.getFilename("resources/huds/hud_Season_Color/hud_summer_Color.png", ssSeasonsMod.modDir), 0, 0, width, height)
+        self.hud.overlays.seasons[ssSeasonsUtil.SEASON_AUTUMN] = Overlay:new("hud_autumn", Utils.getFilename("resources/huds/hud_Season_Color/hud_autumn_Color.png", ssSeasonsMod.modDir), 0, 0, width, height)
+        self.hud.overlays.seasons[ssSeasonsUtil.SEASON_WINTER] = Overlay:new("hud_winter", Utils.getFilename("resources/huds/hud_Season_Color/hud_winter_Color.png", ssSeasonsMod.modDir), 0, 0, width, height)
 
         -- Seasons Weather Icons
         self.hud.overlays.sun = g_currentMission.weatherForecastIconSunOverlay
@@ -93,7 +94,7 @@ function ssWeatherForecast:draw()
             renderText(WeatherForecastPosX + 0.068 + (daysPosOffset * (n - 2)), WeatherForecastPosY + 0.086, 0.02, ssSeasonsUtil:dayNameShort(ssSeasonsUtil:dayOfWeek()+n-1))
 
             -- Render Season Icon
-            renderOverlay(self.hud.overlays[ssSeasonsUtil.seasons[forecast[n].season]].overlayId, WeatherForecastPosX + 0.086 + (daysPosOffset * (n - 2)), WeatherForecastPosY + 0.074, 0.0185, 0.0335)
+            renderOverlay(self.hud.overlays.seasons[forecast[n].season].overlayId, WeatherForecastPosX + 0.086 + (daysPosOffset * (n - 2)), WeatherForecastPosY + 0.074, 0.0185, 0.0335)
 
             -- Render Weather Icon
             renderOverlay(self.hud.overlays[forecast[n].weatherState].overlayId, WeatherForecastPosX + 0.053 + (daysPosOffset * (n - 2)), WeatherForecastPosY + 0.026, 0.032, 0.058)
@@ -131,7 +132,7 @@ function ssWeatherForecast:draw()
     renderOverlay(self.hud.overlays.day_hud.overlayId, dayPosX, dayPosY, g_currentMission.infoBarBgOverlay.height / 2, g_currentMission.infoBarBgOverlay.height)
 
     -- Render Season Icon
-    renderOverlay(self.hud.overlays[ssSeasonsUtil.seasons[forecast[1].season]].overlayId, dayPosX + 0.009 * uiScale, dayPosY + 0.033 * uiScale, 0.0185 * uiScale, 0.0335 * uiScale)
+    renderOverlay(self.hud.overlays.seasons[forecast[1].season].overlayId, dayPosX + 0.009 * uiScale, dayPosY + 0.033 * uiScale, 0.0185 * uiScale, 0.0335 * uiScale)
 
     -- Render current Temperatures
     local currentTemp = mathRound(ssWeatherManager:diurnalTemp(g_currentMission.environment.currentHour, g_currentMission.environment.currentMinute), 0)
