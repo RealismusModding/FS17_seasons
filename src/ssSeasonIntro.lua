e2cec1d2c7892e17b4e6fb4e5eb1d9d871584169
Jos Kuijpers <jkuijpers@jarvix.nl>
2016-12-13 16:13:30 +0100

    Improve seasonsIntro: show only when no GUI is active.

--- src/ssSeasonIntro.lua
+++ src/ssSeasonIntro.lua
@@ -17,6 +17,8 @@ end
 
 function ssSeasonIntro:loadMap(name)
     ssSeasonsMod:addSeasonChangeListener(self)
+
+    self.showSeasonChanged = false
 end
 
 function ssSeasonIntro:deleteMap()
@@ -32,20 +34,15 @@ function ssSeasonIntro:draw()
 end
 
 function ssSeasonIntro:update(dt)
+    if self.showSeasonChanged == true and g_gui.currentGui == nil then
+        self.showSeasonChanged = false
+
+        self:showIntro(ssSeasonsUtil:season())
+    end
 end
 
-function ssSeasonIntro:readStream(streamId, connection)
-    self.hideSeasonIntro = streamReadBool(streamId)
-end
-
-function ssSeasonIntro:writeStream(streamId, connection)
-    streamWriteBool(streamId, self.hideSeasonIntro)
-end
-
-function ssSeasonIntro:seasonChanged()
-    if self.hideSeasonIntro then return end
-
-    local text = ssLang.getText(string.format("SS_SEASON_INTRO_%i", ssSeasonsUtil:season()))
+function ssSeasonIntro:showIntro(season)
+    local text = ssLang.getText(string.format("SS_SEASON_INTRO_%i", season))
     local dialog = g_gui:showDialog("YesNoDialog")
 
     dialog.target:setTitle(ssSeasonsUtil:seasonName())
@@ -61,4 +58,18 @@ function ssSeasonIntro:seasonChanged()
 
         g_gui:closeDialogByName("YesNoDialog")
     end)
+end
+
+function ssSeasonIntro:readStream(streamId, connection)
+    self.hideSeasonIntro = streamReadBool(streamId)
+end
+
+function ssSeasonIntro:writeStream(streamId, connection)
+    streamWriteBool(streamId, self.hideSeasonIntro)
+end
+
+function ssSeasonIntro:seasonChanged()
+    if self.hideSeasonIntro then return end
+
+    self.showSeasonChanged = true
 end
