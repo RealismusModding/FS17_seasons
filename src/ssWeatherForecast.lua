807dc78ffb6792ecfeb594138612ad34b7175de3
Seb <sebastian.me@gmail.com>
2016-12-15 16:53:16 +0000

    Show/hide wf with proper key binding

--- src/ssWeatherForecast.lua
+++ src/ssWeatherForecast.lua
@@ -87,12 +87,16 @@ function ssWeatherForecast:mouseEvent(posX, posY, isDown, isUp, button)
 end
 
 function ssWeatherForecast:keyEvent(unicode, sym, modifier, isDown)
-    if (unicode == 107) then
-        self.hud.visible = not self.hud.visible
-    end
+    -- if (unicode == 107) then
+    --     self.hud.visible = not self.hud.visible
+    -- end
 end
 
 function ssWeatherForecast:update(dt)
+    
+    if InputBinding.hasEvent(InputBinding.SEASONS_SHOW_WF) then
+        self.hud.visible = not self.hud.visible
+    end
 end
 
 function ssWeatherForecast:draw()
