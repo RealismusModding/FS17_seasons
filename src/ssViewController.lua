071bb9ef1cf8d39b87a34ae823710d096f4594bd
Seb <sebastian.me@gmail.com>
2016-12-17 19:15:01 +0000

    Added transition names to debug view

--- src/ssViewController.lua
+++ src/ssViewController.lua
@@ -7,6 +7,23 @@
 
 ssViewController = {}
 
+--currently for debug view only. this should probably go into ssSeasonsUtil and will need translations if we use it for the gui
+ssViewController.growthTransitionIndexToName =
+{
+    [1] = "Early Spring",
+    [2] = "Mid Spring",
+    [3] = "Late Spring",
+    [4] = "Early Summer",
+    [5] = "Mid Summer",
+    [6] = "Late Summer",
+    [7] = "Early Autumn",
+    [8] = "Mid Autumn",
+    [9] = "Late Autumn",
+    [10] = "Early Winter",
+    [11] = "Mid Winter",
+    [12] = "Late Winter"
+}
+
 ssViewController.canPlantDisplayData = {}
 ssViewController.debugView = true
 
@@ -16,6 +33,8 @@ function ssViewController:loadMap(name)
 
     self:growthStageChanged()
     self:dayChanged()
+    --print_r(self.growthTransitionIndexToName)
+    --print(self.growthTransitionIndexToName[1])
     --print_r(self.canPlantDisplayData)
     --log("drygrass: " .. FruitUtil.fruitTypeGrowths["dryGrass"].name)
 end
@@ -40,12 +59,12 @@ end
 function ssViewController:update(dt)
 end
 
-
 function ssViewController:draw()
     if self.debugView == true then
         renderText(0.54, 0.98, 0.01, "GM enabled: " .. tostring(ssGrowthManager.growthManagerEnabled) .. " doGrowthTransition: " .. tostring(ssGrowthManager.doGrowthTransition))
-        local growthTransition = tostring(ssSeasonsUtil:currentGrowthTransition())
-        renderText(0.54, 0.96, 0.01, "Growth Transition: " .. growthTransition)
+        local growthTransition = ssSeasonsUtil:currentGrowthTransition()
+        
+        renderText(0.54, 0.96, 0.01, "Growth Transition: " .. growthTransition .. " " .. self.growthTransitionIndexToName[growthTransition])
         local cropsThatCanGrow = ""
         
         for index,fruit in pairs(g_currentMission.fruits) do
@@ -87,3 +106,5 @@ end
 function ssViewController:canSow() -- dummy function until it's implemented in WM
     return ssGrowthManager.TRUE
 end
+
+
