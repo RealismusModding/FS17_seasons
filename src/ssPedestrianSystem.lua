393ac7a3a90288fb67a1de077c16b8774759f9a4
Jos Kuijpers <jos@kuijpersvof.nl>
2017-01-08 23:55:09 +0100

    Do not spawn pedestrians in the winter

--- src/ssPedestrianSystem.lua
+++ src/ssPedestrianSystem.lua
@@ -10,6 +10,8 @@ ssPedestrianSystem = {}
 function ssPedestrianSystem:loadMap(name)
     ssSeasonsMod:addSeasonChangeListener(self)
 
+    PedestrianSystem.update = Utils.overwrittenFunction(PedestrianSystem.update, ssPedestrianSystem.originalUpdate)
+
     if g_currentMission:getIsServer() then
         -- Initial setuo (it changed from nothing)
         self:seasonChanged()
@@ -39,9 +41,15 @@ end
 function ssPedestrianSystem:seasonChanged()
     local season = ssSeasonsUtil:season()
 
-    if season == ssSeasonsUtil.SEASON_WINTER then
-        g_currentMission.pedestrianSystem:setEnabled(false)
-    else
-        g_currentMission.pedestrianSystem:setEnabled(true)
+    self.showPedestrians = not (season == ssSeasonsUtil.SEASON_WINTER)
+end
+
+function ssPedestrianSystem.originalUpdate(pedestrianSystem, dt)
+    local dayTime = g_currentMission.environment.dayTime
+
+    if not ssPedestrianSystem.showPedestrians then
+        dayTime = 0 -- midnight, do not spawn
     end
+
+    setPedestrianSystemDaytime(pedestrianSystem.pedestrianSystemId, dayTime);
 end
