8b344fba1aca8da56a187be9344df16c88b0a093
Jos Kuijpers <jos@kuijpersvof.nl>
2017-02-01 22:36:41 +0100

    Add menu item for Celcius/Fahrenheit. Refactor fahrenheit a bit

--- src/ssLang.lua
+++ src/ssLang.lua
@@ -15,3 +15,16 @@ function ssLang.getText(key, default)
         return key
     end
 end
+
+--- function to convert from Celsius to Fahrenheit
+function ssLang.formatTemperature(tempCelcius)
+    if ssWeatherForecast.degreeFahrenheit then
+        return string.format("%iºF", ssLang.convertTempToFahrenheit(tempCelcius))
+    else
+        return string.format("%iºC", tempCelcius)
+    end
+end
+
+function ssLang.convertTempToFahrenheit(tempCelcius)
+    return mathRound(tempCelcius * 1.8 + 32, 0)
+end
