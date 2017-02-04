38ee730075222993163c099eaeff94dcd27f4176
reallogger <fsgamelogger@gmail.com>
2017-02-04 17:12:28 +0100

    commenting ssSnowFillable

--- src/ssSnowFillable.lua
+++ src/ssSnowFillable.lua
@@ -27,21 +27,21 @@ function ssSnowFillable:update(dt)
 end
 
 function ssSnowFillable:updateTick(dt)
-    if self.isServer then
-        local level = self:getFillLevel(FillUtil.FILLTYPE_SNOW)
-        local temp = g_seasons.weather:currentTemperature()
-
-        local diff = 0
-
-        if level > 0 and temp > 0 then
-            diff = -1 * level * 0.0001 * (dt / 1000) -- To be made correctly by reallogger
-
-            -- FIXME(jos): with multiple units, too much (or less) is removed. Need to do this all per unit.
-            local units = self:getFillUnitsWithFillType(FillUtil.FILLTYPE_SNOW)
-            for _, fillUnit in pairs(units) do
-                self:setUnitFillLevel(fillUnit.fillUnitIndex, level + diff, FillUtil.FILLTYPE_SNOW)
-            end
-        end
+--    if self.isServer then
+--        local level = self:getFillLevel(FillUtil.FILLTYPE_SNOW)
+--        local temp = g_seasons.weather:currentTemperature()
+--
+--        local diff = 0
+--
+--        if level > 0 and temp > 0 then
+--            diff = -1 * level * 0.0001 * (dt / 1000) -- To be made correctly by reallogger
+--
+--            -- FIXME(jos): with multiple units, too much (or less) is removed. Need to do this all per unit.
+--            local units = self:getFillUnitsWithFillType(FillUtil.FILLTYPE_SNOW)
+--            for _, fillUnit in pairs(units) do
+--                self:setUnitFillLevel(fillUnit.fillUnitIndex, level + diff, FillUtil.FILLTYPE_SNOW)
+--            end
+--        end
 
         -- if temp < 0 and IT SNOWS AND (is snow OR is empty) then
         --     -- If the cover is closed it should automatically not add snow, as no filltype is then
@@ -50,7 +50,7 @@ function ssSnowFillable:updateTick(dt)
 
         --     self:setFillLevel(level + diff, FillUtil.FILLTYPE_SNOW)
         -- end
-    end
+--    end
 end
 
 function ssSnowFillable:draw()
