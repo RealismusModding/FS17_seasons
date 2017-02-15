f77397425622d85093b2c1b36ca9e34d92159280
Jos Kuijpers <jos@kuijpersvof.nl>
2017-02-15 01:04:51 +0100

    Clean up snow fill code. Implement filling with snow (untested)

--- src/ssSnowFillable.lua
+++ src/ssSnowFillable.lua
@@ -39,29 +39,34 @@ local function vehicleHasFillType(vehicle, type)
 end
 
 function ssSnowFillable:updateTick(dt)
-   if self.isServer then
-       local temp = g_seasons.weather:currentTemperature()
+    if self.isServer then
+        local temp = g_seasons.weather:currentTemperature()
 
         if vehicleHasFillType(self, FillUtil.FILLTYPE_SNOW) and temp > 0 then
             local level = self:getFillLevel(FillUtil.FILLTYPE_SNOW)
             local diff = -1 * level * 0.0001 * (dt / 1000) -- To be made correctly by reallogger
 
-            -- FIXME(jos): with multiple units, too much (or less) is removed. Need to do this all per unit.
+            -- Update each unit
             local units = self:getFillUnitsWithFillType(FillUtil.FILLTYPE_SNOW)
             for _, fillUnit in pairs(units) do
-                self:setUnitFillLevel(fillUnit.fillUnitIndex, level + diff, FillUtil.FILLTYPE_SNOW)
+                local unitLevel = self:getUnitFillLevel(fillUnit.fillUnitIndex)
+
+                -- Add each compartment the same amount of snow (not based on capacity). Don't bother with the details.
+                self:setUnitFillLevel(fillUnit.fillUnitIndex, unitLevel + diff / table.getn(units), FillUtil.FILLTYPE_SNOW)
             end
         end
---[[
-        if temp < 0 and IT SNOWS AND (is snow OR is empty) then
-            -- If the cover is closed it should automatically not add snow, as no filltype is then
-            -- 'allowed'
-            diff = level * 0.0001 * (dt / 1000) -- To be made correctly by reallogger
 
+        if temp < 0 and vehicle:allowFillFromAir() -- For cover
+              and g_currentMission.environment.currentRain ~= nil
+              and g_currentMission.environment.currentRain.rainTypeId == Environment.RAINTYPE_SNOW then
+            local level = self:getFillLevel(FillUtil.FILLTYPE_SNOW)
+            local diff = level * 0.0001 * (dt / 1000) -- To be made correctly by reallogger
+
+            -- Use this, without force to not override current fills.
+            -- Jos: I am not sure this works.. It did not seem to for the emptying.. WIP, needs testing
             self:setFillLevel(level + diff, FillUtil.FILLTYPE_SNOW)
         end
-]]
-   end
+    end
 end
 
 function ssSnowFillable:draw()
