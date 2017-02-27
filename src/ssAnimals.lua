a9398a5330c634f4caf7da26983d817b2f236f87
reallogger <fsgamelogger@gmail.com>
2017-02-27 21:41:03 +0100

    remove print

--- src/ssAnimals.lua
+++ src/ssAnimals.lua
@@ -27,7 +27,6 @@ function ssAnimals:load()
     -- adjust animal values for varying season length
     -- reference season length is 6 days
     self.seasonLengthfactor = 6 / g_seasons.environment.daysInSeason
-    log(self.seasonLengthfactor)
 end
 
 function ssAnimals:loadFromXML()
