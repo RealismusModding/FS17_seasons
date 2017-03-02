0c947786063261ada94e63e5ad893295aa7d4741
SebastianP <sebastian@7kss.com>
2017-03-02 20:44:01 +0000

    Made mission disabling toggleable via savegame

--- src/ssFieldJobManager.lua
+++ src/ssFieldJobManager.lua
@@ -8,9 +8,21 @@
 ssFieldJobManager = {}
 g_seasons.fieldJobManager = ssFieldJobManager
 
+ssFieldJobManager.disableMissions = true
+
+function ssFieldJobManager:load(savegame, key)
+    self.disableMissions = ssStorage.getXMLBool(savegame, key .. ".settings.disableMissions", true)
+end
+
+function ssFieldJobManager:save(savegame, key)
+    if g_currentMission:getIsServer() == true then
+        ssStorage.setXMLBool(savegame, key .. ".settings.disableMissions", self.disableMissions)
+    end
+end
+
 function ssFieldJobManager:loadMap(name)
     for _,fieldDef in pairs(g_currentMission.fieldDefinitionBase.fieldDefs) do
-        fieldDef.fieldJobUsageAllowed = false
+        fieldDef.fieldJobUsageAllowed = not self.disableMissions
     end
 end
 
