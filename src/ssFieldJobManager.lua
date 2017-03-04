c31a77f5a14994adecbdd4826f56368b47e4f740
Jos Kuijpers <jos@kuijpersvof.nl>
2017-03-04 20:46:27 +0100

    Clean up disabling of missions

--- src/ssFieldJobManager.lua
+++ src/ssFieldJobManager.lua
@@ -8,21 +8,17 @@
 ssFieldJobManager = {}
 g_seasons.fieldJobManager = ssFieldJobManager
 
-ssFieldJobManager.disableMissions = true
-
 function ssFieldJobManager:load(savegame, key)
     self.disableMissions = ssStorage.getXMLBool(savegame, key .. ".settings.disableMissions", true)
 end
 
 function ssFieldJobManager:save(savegame, key)
-    if g_currentMission:getIsServer() == true then
-        ssStorage.setXMLBool(savegame, key .. ".settings.disableMissions", self.disableMissions)
-    end
+    ssStorage.setXMLBool(savegame, key .. ".settings.disableMissions", self.disableMissions)
 end
 
 function ssFieldJobManager:loadMap(name)
     for _,fieldDef in pairs(g_currentMission.fieldDefinitionBase.fieldDefs) do
-        fieldDef.fieldJobUsageAllowed = not self.disableMissions
+        fieldDef.fieldJobUsageAllowed = not self.disableMissions and fieldDef.fieldJobUsageAllowed
     end
 end
 
