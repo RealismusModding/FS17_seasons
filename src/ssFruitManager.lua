18ed0f446a19395489704a77859c21b782c4856d
SebastianP <sebastian@7kss.com>
2017-03-05 15:01:31 +0000

    FruitManager: finished implementation Remembered that in lua it's ~= and not !=

--- src/ssFruitManager.lua
+++ src/ssFruitManager.lua
@@ -23,11 +23,19 @@ end
 function ssFruitManager:updateHarvestStages()
     for index,fruit in pairs(g_currentMission.fruits) do
         local fruitName = FruitUtil.fruitIndexToDesc[index].name
-        if index != FruitUtil.FRUITTYPE_POPLAR and index != FruitUtil.FRUITTYPE_OILSEEDRADISH and index != FruitUtil.FRUITTYPE_DRYGRASS
-                and index != FruitUtil.FRUITTYPE_SUGARBEET and index != FruitUtil.FRUITTYPE_POTATO then
+        if self:fruitShouldBeUpdated(index) == true then
             FruitUtil.fruitIndexToDesc[index].minHarvestingGrowthState = FruitUtil.fruitIndexToDesc[index].maxHarvestingGrowthState
         end
     end
+end
+
+function ssFruitManager:fruitShouldBeUpdated(index)
+    if index ~= FruitUtil.FRUITTYPE_POPLAR and index ~= FruitUtil.FRUITTYPE_OILSEEDRADISH and index ~= FruitUtil.FRUITTYPE_DRYGRASS 
+                and index ~= FruitUtil.FRUITTYPE_SUGARBEET and index ~= FruitUtil.FRUITTYPE_POTATO then
+        return true
+    end
+
+    return false
 end
 
 
