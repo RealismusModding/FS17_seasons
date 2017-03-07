5a31747e07a90d0c42c3f20eb1e166acebaf8b7b
Jos Kuijpers <jos@kuijpersvof.nl>
2017-03-08 00:43:33 +0100

    Make the icon show (not good looking yet) (#179)

--- src/environment/ssSnow.lua
+++ src/environment/ssSnow.lua
@@ -50,7 +50,10 @@ function ssSnow:loadMap(name)
     local t = FillUtil.registerFillType("snow", g_i18n:getText("fillType_snow"), FillUtil.FILLTYPE_CATEGORY_BULK, 0, false, g_seasons.modDir .. "resources/huds/hud_fill_snow.png", g_seasons.modDir .. "resources/huds/hud_fill_snow_sml.png", 0.00016, math.rad(50))
     TipUtil.registerDensityMapHeightType(FillUtil.FILLTYPE_SNOW, math.rad(35), 0.8, 0.10, 0.10, 1.20, 3, true, g_seasons.modDir .. "resources/environment/snow_diffuse.dds", g_seasons.modDir .. "resources/environment/snow_normal.dds", g_seasons.modDir .. "resources/environment/snowDistance_diffuse.dds")
 
-    print_r(FillUtil.fillTypeNameToDesc["snow"])
+    -- Load overlay icon, properly
+    local uiScale = g_gameSettings:getValue("uiScale")
+    local levelIconWidth, levelIconHeight = getNormalizedScreenValues(20 * uiScale, 20 * uiScale)
+    g_currentMission:addFillTypeOverlay(t, FillUtil.fillTypeIndexToDesc[t].hudOverlayFilename, levelIconWidth, levelIconHeight)
 
     if g_currentMission:getIsServer() then
         g_currentMission.environment:addHourChangeListener(self)
