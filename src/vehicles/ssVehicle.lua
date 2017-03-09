fd1322e28c764e52a50e67ff20168b47fe798cb2
reallogger <fsgamelogger@gmail.com>
2017-03-09 15:45:19 +0100

    changes to ssVehicle (not yet working)

--- src/vehicles/ssVehicle.lua
+++ src/vehicles/ssVehicle.lua
@@ -496,3 +496,147 @@ function ssVehicle:getIsThreshingAllowed(superFunc,earlyWarning)
 
     return not g_seasons.weather:isCropWet()
 end
+
+-- not working atm
+function ssVehicle:processTedderAreas(superFunc, workAreas, accumulatedWorkAreaValues)
+    local retWorkAreas =  superFunc(self, workAreas, accumulatedWorkAreaValues)
+
+    local numAreas = table.getn(workAreas)
+
+    --local retWorkAreas = {}
+    for i=1, numAreas do
+        local x0 = workAreas[i][1]
+        local z0 = workAreas[i][2]
+        local x1 = workAreas[i][3]
+        local z1 = workAreas[i][4]
+        local x2 = workAreas[i][5]
+        local z2 = workAreas[i][6]
+        local dx0 = workAreas[i][7]
+        local dz0 = workAreas[i][8]
+        local dx1 = workAreas[i][9]
+        local dz1 = workAreas[i][10]
+        local dx2 = workAreas[i][11]
+        local dz2 = workAreas[i][12]
+
+        -- pick up
+        local hx = x2 - x0
+        local hz = z2 - z0
+        local hLength = Utils.vector2Length(hx, hz)
+        local hLength_2 = 0.5 * hLength
+
+        local wx = x1 - x0
+        local wz = z1 - z0
+        local wLength = Utils.vector2Length(wx, wz)
+
+        local sx = x0 + (hx * 0.5) + ((wx/wLength)*hLength_2)
+        local sz = z0 + (hz * 0.5) + ((wz/wLength)*hLength_2)
+
+        local ex = x1 + (hx * 0.5) - ((wx/wLength)*hLength_2)
+        local ez = z1 + (hz * 0.5) - ((wz/wLength)*hLength_2)
+
+        local sy = getTerrainHeightAtWorldPos(g_currentMission.terrainRootNode, sx,0,sz)
+        local ey = getTerrainHeightAtWorldPos(g_currentMission.terrainRootNode, ex,0,ez)
+
+        local fillType1 = FruitUtil.fruitTypeToWindrowFillType[FruitUtil.FRUITTYPE_GRASS]
+        local liters1 = TipUtil.tipToGroundAroundLine(self, -math.huge, fillType1, sx,sy,sz, ex,ey,ez, hLength_2, nil, nil, false, nil)
+
+        local fillType2 = g_seasons.weather:isCropWet() and FruitUtil.FRUITTYPE_GRASS or FruitUtil.FRUITTYPE_DRYGRASS
+        local liters2 = TipUtil.tipToGroundAroundLine(self, -math.huge, fillType2, sx,sy,sz, ex,ey,ez, hLength_2, nil, nil, false, nil)
+
+        local liters = -liters1 - liters2
+
+        -- drop
+        local hx = dx2 - dx0
+        local hz = dz2 - dz0
+        local hLength = Utils.vector2Length(hx, hz)
+        local hLength_2 = 0.5 * hLength
+
+        local wx = dx1 - dx0
+        local wz = dz1 - dz0
+        local wLength = Utils.vector2Length(wx, wz)
+
+        local sx = dx0 + (hx * 0.5) + ((wx/wLength)*hLength_2)
+        local sz = dz0 + (hz * 0.5) + ((wz/wLength)*hLength_2)
+
+        local ex = dx1 + (hx * 0.5) - ((wx/wLength)*hLength_2)
+        local ez = dz1 + (hz * 0.5) - ((wz/wLength)*hLength_2)
+
+        local sy = getTerrainHeightAtWorldPos(g_currentMission.terrainRootNode, sx,0,sz)
+        local ey = getTerrainHeightAtWorldPos(g_currentMission.terrainRootNode, ex,0,ez)
+
+        local toDrop = accumulatedWorkAreaValues[i] + liters
+        
+        local fillType = g_seasons.weather:isCropWet() and FruitUtil.FRUITTYPE_GRASS or FruitUtil.FRUITTYPE_DRYGRASS
+        local dropped, lineOffset = TipUtil.tipToGroundAroundLine(self, toDrop, FruitUtil.fruitTypeToWindrowFillType[fillType], sx,sy,sz, ex,ey,ez, hLength_2, nil, self.tedderLineOffset, false, nil, false)
+
+        --local dropped, lineOffset = TipUtil.tipToGroundAroundLine(self, toDrop, FruitUtil.fruitTypeToWindrowFillType[FruitUtil.FRUITTYPE_DRYGRASS], sx,sy,sz, ex,ey,ez, hLength_2, nil, self.tedderLineOffset, false, nil, false)
+        self.tedderLineOffset = lineOffset
+        local remain = toDrop - dropped
+
+        accumulatedWorkAreaValues[i] = remain
+        workAreas[i][13] = remain
+
+        if liters > remain then
+            table.insert(retWorkAreas, workAreas[i])
+        end
+
+    end
+    return retWorkAreas
+end
+
+-- not working atm 
+function ssVehicle:startMotor(superFunc, noEventSend)
+    superFunc(self, noEventSend)
+    
+    if noEventSend == nil or noEventSend == false then
+        if g_server ~= nil then
+            g_server:broadcastEvent(SetMotorTurnedOnEvent:new(self, true), nil, nil, self)
+        else
+            g_client:getServerConnection():sendEvent(SetMotorTurnedOnEvent:new(self, true))
+        end
+    end
+
+    local overdueFactor = 1
+
+    if SpecializationUtil.hasSpecialization(repairable, self.specializations) then
+        overdueFactor = ssVehicle:calculateOverdueFactor(self)
+    end
+
+    -- for testing. 
+    overdueFactor = 2
+
+    if not self.isMotorStarted then
+        self.isMotorStarted = true
+
+        if self.isClient then
+            if self.exhaustParticleSystems ~= nil then
+                for _, ps in pairs(self.exhaustParticleSystems) do
+                    ParticleUtil.setEmittingState(ps, true)
+                end
+            end
+            --Probably  need to find a way to loop the sound several times. Not sure if it is done below. 
+            if self:getIsActiveForSound() then
+                SoundUtil.playSample(self.sampleMotorStart, 1, 0, nil)
+            end
+            if self.exhaustEffects ~= nil then
+                for _, effect in pairs(self.exhaustEffects) do
+                    setVisibility(effect.effectNode, true)
+                    effect.xRot = effect.xzRotationsOffset[1]
+                    effect.zRot = effect.xzRotationsOffset[2]
+                    setShaderParameter(effect.effectNode, "param", effect.xRot, effect.zRot, 0, 0, false)
+
+                    local color = effect.minRpmColor
+                    setShaderParameter(effect.effectNode, "exhaustColor", color[1], color[2], color[3], color[4], false)
+                end
+            end
+        end
+
+        self.motorStartTime = g_currentMission.time + self.motorStartDuration * overdueFactor
+        self.compressionSoundTime = g_currentMission.time + math.random(5000, 20000)
+        self.lastRoundPerMinute=0
+
+        if self.fuelFillLevelHud ~= nil then
+            VehicleHudUtils.setHudValue(self, self.fuelFillLevelHud, self.fuelFillLevel, self.fuelCapacity)
+        end
+    end
+end
