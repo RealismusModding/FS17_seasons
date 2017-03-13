b04fa6898a1d51dd7bca8d9e7022bf13a30db511
SebastianP <sebastian@7kss.com>
2017-03-13 19:06:40 +0000

    Arghghg

--- src/misc/ssFieldJobManager.lua
+++ src/misc/ssFieldJobManager.lua
@@ -17,7 +17,7 @@ function ssFieldJobManager:save(savegame, key)
 end
 
 function ssFieldJobManager:loadMap(name)
-   if not (fieldDefinitionBase ~= nil and fieldDefs ~= nil) then return end
+   if not (g_currentMission.fieldDefinitionBase ~= nil and g_currentMission.fieldDefinitionBase.fieldDefs ~= nil) then return end
 
     for _,fieldDef in pairs(g_currentMission.fieldDefinitionBase.fieldDefs) do
         fieldDef.fieldJobUsageAllowed = not self.disableMissions and fieldDef.fieldJobUsageAllowed
