6393ef636c9848f8216cb5f2e1adf8665ff756be
Jos Kuijpers <jos@kuijpersvof.nl>
2017-03-31 19:52:31 +0200

    Improve BunkerSilo adjustment. Now also works when changing season length

--- src/objects/ssBunkerSilo.lua
+++ src/objects/ssBunkerSilo.lua
@@ -11,6 +11,10 @@ ssBunkerSilo = {}
 
 function ssBunkerSilo:preLoad()
     BunkerSilo.loadFromAttributesAndNodes = Utils.overwrittenFunction(BunkerSilo.loadFromAttributesAndNodes, ssBunkerSilo.bunkerSiloLoadFromAttributesAndNodes)
+
+    BunkerSilo.delete = Utils.overwrittenFunction(BunkerSilo.delete, ssBunkerSilo.bunkerSiloDelete)
+
+    BunkerSilo.seasonChanged = ssBunkerSilo.bunkerSiloSeasonChanged
 end
 
 function ssBunkerSilo:loadMap()
@@ -19,8 +23,17 @@ end
 function ssBunkerSilo:bunkerSiloLoadFromAttributesAndNodes(superFunc, xmlFile, key)
     self.fermentingDuration = g_seasons.environment.daysInSeason / 3 * 24 * 60 * 60 -- '4 weeks'
 
+    g_seasons.environment:addSeasonLengthChangeListener(self)
+
     return superFunc(self, xmlFile, key)
 end
 
--- FIXME: When season length changes this breaks
--- Solution: append load() and delete(), and add/remove a seasonChangeListener
+function ssBunkerSilo:bunkerSiloDelete(superFunc)
+    superFunc(self)
+
+    g_seasons.environment:removeSeasonLengthChangeListener(self)
+end
+
+function ssBunkerSilo:bunkerSoloSeasonChanged()
+    self.fermentingDuration = g_seasons.environment.daysInSeason / 3 * 24 * 60 * 60 -- '4 weeks'
+end
