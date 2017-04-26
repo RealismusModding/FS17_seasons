923f7df9aa29d180cd01fb45d8643d44dd5c2017
Jos Kuijpers <jos@kuijpersvof.nl>
2017-04-26 13:52:35 +0200

    Create a filler for allowing invisible, collidable objects when seasons is not loaded

---
+++ modding/seasons_filler.lua
@@ -0,0 +1,28 @@
+----------------------------------------------------------------------------------------------------
+-- MAP COMPATIBILITY
+----------------------------------------------------------------------------------------------------
+-- Purpose:  Filler functions that are used when Seasons is not loaded.
+-- Authors:  Rahkiin
+--
+-- Without this script, objects that are set to be invisible by default, when seasons is not
+-- loaded, will still have an active collision mask. So this code creates the admirers and
+-- when it is set invisible, it disables the collision mask as well.
+--
+-- Copyright (c) Realismus Modding, 2017
+----------------------------------------------------------------------------------------------------
+
+-- Always create the objects.
+-- We can't properly detect if Seasons is loaded, because
+-- that depends on the load order.
+local fn = function (self, id)
+    if getVisibility(id) == false then
+        setCollisionMask(id, 0)
+    end
+end
+
+local class = {}
+class.onCreate = fn
+
+getfenv(0)["ssIcePlane"] = class
+getfenv(0)["ssSeasonAdmirer"] = class
+getfenv(0)["ssSnowAdmirer"] = class
