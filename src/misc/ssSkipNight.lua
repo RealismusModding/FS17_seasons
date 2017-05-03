d2dd257df25e5b9fc577beda802798cbd0483835
Jos Kuijpers <jos@kuijpersvof.nl>
2017-05-03 08:36:46 +0200

    Fix possible issue with skip night. Lower speed. Force.
    If skip night is started you can't stop it, you will be skipping night.

--- src/misc/ssSkipNight.lua
+++ src/misc/ssSkipNight.lua
@@ -10,7 +10,7 @@
 ssSkipNight = {}
 g_seasons.skipNight = ssSkipNight
 
-ssSkipNight.SPEED = 10000
+ssSkipNight.SPEED = 6000
 
 function ssSkipNight:loadMap()
     self.skippingNight = false
@@ -27,11 +27,16 @@ function ssSkipNight:update(dt)
     if isEvening then
         g_currentMission:addHelpButtonText(g_i18n:getText("input_SEASONS_SKIP_NIGHT"), InputBinding.SEASONS_SKIP_NIGHT)
 
+        -- When a player wants to skip the night, fast forward, securily
         if InputBinding.hasEvent(InputBinding.SEASONS_SKIP_NIGHT) then
             self.skippingNight = true
+            self.minuteCount = g_currentMission.environment.currentMinute
+
+            g_currentMission.environment:addMinuteChangeListener(self)
+            g_currentMission.environment:addHourChangeListener(self)
 
             self.oldTimeScale = g_currentMission.missionInfo.timeScale
-            g_currentMission.missionInfo.timeScale = ssSkipNight.SPEED
+            g_currentMission:setTimeScale(ssSkipNight.SPEED)
         end
     end
 
@@ -40,6 +45,31 @@ function ssSkipNight:update(dt)
     if self.skippingNight and isMorning then
         self.skippingNight = false
 
-        g_currentMission.missionInfo.timeScale = self.oldTimeScale
+        g_currentMission.environment:removeMinuteChangeListener(self)
+        g_currentMission.environment:removeHourChangeListener(self)
+
+        g_currentMission:setTimeScale(self.oldTimeScale)
     end
+
+    -- Ignore any change in timescale by the player
+    if self.skippingNight and g_currentMission.missionInfo.timeScale ~= ssSkipNight.SPEED then
+        g_currentMission:setTimeScale(ssSkipNight.SPEED)
+    end
+end
+
+function ssSkipNight:hourChanged()
+    if self.minuteCount < 60 then
+        -- For every minute not having a minute callback, add an extra callback
+        for i = 1, 60 - self.minuteCount, 1 do
+            for _, listener in pairs(g_currentMission.environment.minuteChangeListeners) do
+                listener:minuteChanged()
+            end
+        end
+    end
+
+    self.minuteCount = 0
+end
+
+function ssSkipNight:minuteChanged()
+    self.minuteCount = self.minuteCount + 1
 end
