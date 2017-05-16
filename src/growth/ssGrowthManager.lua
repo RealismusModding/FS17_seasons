7e0a0da656995aef3c2426f93beca5ac506d644f
SebastianP <sebastian@7kss.com>
2017-05-16 08:00:33 +0100

    More reset for testing

--- src/growth/ssGrowthManager.lua
+++ src/growth/ssGrowthManager.lua
@@ -252,6 +252,7 @@ function ssGrowthManager:setGrowthState(fruit, fruitName, x, z, widthX, widthZ, 
 
     local numFruitStateChannels = g_currentMission.numFruitStateChannels
     local growthResult = setDensityMaskedParallelogram(fruit.id, x, z, widthX, widthZ, heightX, heightZ, 0, numFruitStateChannels, fruit.id, 0, numFruitStateChannels, desiredGrowthState)
+    setDensityCompareParams(fruit.id, "greater", -1)
     setDensityMaskParams(fruit.id, "greater", -1) -- reset
 end
 
@@ -295,6 +296,7 @@ function ssGrowthManager:incrementGrowthState(fruit, fruitName, x, z, widthX, wi
             setDensityCompareParams(terrainDetailId, "greater", -1) -- reset
         end
     end
+    setDensityCompareParams(fruit.id, "greater", -1)
     setDensityMaskParams(fruit.id, "greater", -1) -- reset
 end
 
@@ -316,6 +318,7 @@ function ssGrowthManager:incrementExtraGrowthState(fruit, fruitName, x, z, width
             local sprayResetResult = setDensityMaskedParallelogram(terrainDetailId, x, z, widthX, widthZ, heightX, heightZ, g_currentMission.sprayFirstChannel, g_currentMission.sprayNumChannels, fruit.id, 0, numFruitStateChannels, 0)
         end
     end
+    setDensityCompareParams(fruit.id, "greater", -1)
     setDensityMaskParams(fruit.id, "greater", -1) -- reset
 end
 
