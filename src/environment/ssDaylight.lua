70ed7b9325dae51c45a421c3b93327cad57dcc36
reallogger <fsgamelogger@gmail.com>
2017-10-23 00:31:30 +0200

    ssDaylight change comment text

--- src/environment/ssDaylight.lua
+++ src/environment/ssDaylight.lua
@@ -101,7 +101,7 @@ end
 function ssDaylight:setupDayNight()
     -- Calculate some constants for the daytime calculator
     self.sunRad = self.latitude * math.pi / 180
-    self.pNight = 18 * math.pi / 180 -- Suns inclination below the horizon for 'civil twilight'
+    self.pNight = 18 * math.pi / 180 -- Suns inclination below the horizon for 'astronomical twilight'
     self.pDay = -1 * math.pi / 180 -- Suns inclination above the horizon for 'daylight' assumed to be one degree above horizon
 
     -- Update time before game start to prevent sudden change of darkness
