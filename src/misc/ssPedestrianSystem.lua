beca6194f82aeaf525a209335a070681e57b75cc
Jos Kuijpers <jkuijpers@jarvix.nl>
2017-10-24 10:21:31 +0200

    Fix bad name

--- src/misc/ssPedestrianSystem.lua
+++ src/misc/ssPedestrianSystem.lua
@@ -17,7 +17,7 @@ end
 function ssPedestrianSystem:psUpdate(superFunc, dt)
     local dayTime = g_currentMission.environment.dayTime
 
-    if g_seasons.weatherManager:currentTemperature() < 5 then
+    if g_seasons.weather:currentTemperature() < 5 then
         dayTime = 0 -- midnight, do not spawn
     end
 
