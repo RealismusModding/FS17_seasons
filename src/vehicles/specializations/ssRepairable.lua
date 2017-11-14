d55d1494bc0cf930c1e6eccc6d9b042ed16d6cae
Jos Kuijpers <jkuijpers@jarvix.nl>
2017-11-14 10:26:32 +0100

    Slightly increase player range for repairing

--- src/vehicles/specializations/ssRepairable.lua
+++ src/vehicles/specializations/ssRepairable.lua
@@ -9,6 +9,7 @@
 ssRepairable = {}
 
 ssRepairable.MAX_CHARS_TO_DISPLAY = 20
+ssRepairable.PLAYER_RANGE = 5.0
 
 source(g_seasons.modDir .. "src/events/ssRepairVehicleEvent.lua")
 
@@ -96,7 +97,7 @@ end
 function ssRepairable:updateTick(dt)
     -- Calculate if vehicle is in range for message about repairing
     if self.isClient and g_currentMission.controlPlayer and g_currentMission.player ~= nil then
-        local isPlayerInRange, player = getIsPlayerInRange(self, 4.0, g_currentMission.player)
+        local isPlayerInRange, player = getIsPlayerInRange(self, ssRepairable.PLAYER_RANGE, g_currentMission.player)
 
         if isPlayerInRange then
             self.ssPlayerInRange = player
